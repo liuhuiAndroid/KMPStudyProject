04f6677b895f81bdb8f2cf5d440abe5111eb24d3
DESKTOP-4M6AVA3\seckill <im.lh@hotmail.com>
2025-06-05 21:55:39 +0800

    modify ProtobufTest

--- ProtobufTest/app/build.gradle.kts
+++ ProtobufTest/app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.protobuf.kotlinlite)
+    implementation(libs.protobuf.kotlin)
 }
 
 // ./gradlew generateReleaseProto
--- ProtobufTest/app/src/main/java/com/protobuf/test/MainActivity.kt
+++ ProtobufTest/app/src/main/java/com/protobuf/test/MainActivity.kt
@@ -6,18 +6,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.protobuf.test.ui.theme.ProtobufTestTheme
 import erdai.StudentOuterClass
+import erdai.student
 
 /**
  * https://juejin.cn/post/7135365943282122765
- * https://www.youtube.com/watch?v=LtvDeSX5U0s
  */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,8 +29,7 @@ class MainActivity : ComponentActivity() {
             ProtobufTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        greeting = "Android", modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -37,34 +38,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(greeting: String, modifier: Modifier = Modifier) {
     Button(onClick = {
-//        val student = student {
-//            this.name = "erdai"
-//            this.age = 18
-//            this.email = "erdai666@qq.com"
-//            this.course += "Math"
-//            this.course += "English"
-//            this.course += "Computer"
-//        }
-        val student = StudentOuterClass.Student.newBuilder()
-            .setName("erdai")
-            .setAge(18)
-            .setEmail("erdai666@qq.com")
-            .addAllCourse(mutableListOf("Math", "English", "Computer"))
+        val student0 = student {
+            name = "erdai"
+            age = 18
+            email = "erdai666@qq.com"
+            course += "Math"
+            course += "English"
+            course += "Computer"
+        }
+        val student = StudentOuterClass.Student.newBuilder().setName("erdai").setAge(18)
+            .setEmail("erdai666@qq.com").addAllCourse(mutableListOf("Math", "English", "Computer"))
             .build()
         Log.i("TAG", "原始对象:$student")
         // 2. 序列化为字节数组
         val byteArray: ByteArray = student.toByteArray()
-        Log.i("TAG", "序列化为字节数组:$student")
-
+        Log.i("TAG", "序列化为字节数组:${String(byteArray)}")
         // 3. 反序列化为 Protobuf 对象
         val parsedStudent = StudentOuterClass.Student.parseFrom(byteArray)
-        Log.i("TAG", "反序列化对象:$parsedStudent")
-    }) {
+        // Log.i("TAG", "反序列化对象:$parsedStudent")
+        Log.i("TAG", "反序列化对象name:${parsedStudent.name}")
+        Log.i("TAG", "反序列化对象age:${parsedStudent.age}")
+    }, modifier = modifier.height(48.dp)) {
         Text(
-            text = "Hello $name!",
-            modifier = modifier
+            text = "Hello $greeting!"
         )
     }
 }
